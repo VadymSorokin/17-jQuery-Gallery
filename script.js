98d840c6cca357b7cb424a1b46860a52b6510510
VadymSorokin <s.a.rock.in619@gmail.com>
2021-03-18 19:33:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,65 @@
-const albumList = $('.album__list');
-const albumPhotos = $('.album__photos');
+const $albumList = $('.album__list');
+const $albumPhotos = $('.album__photos');
 
 function init() {
-	renderAlbums();
-	renderPhotos();
+	renderFirstAlbumPhotos();
+	getAndRenderAlbums();
+	getAndRenderPhotos();
 	renderAlbumPhotoEventListener();
 }
 init()
+
+function getAndRenderAlbums() {
+	sendGetRequestAlbums()
+		.then((albums) => createAlbumItem(albums))
+}
 
 function sendGetRequestAlbums() {
 	return fetch('https://jsonplaceholder.typicode.com/albums')
 		.then((response) => response.json())
 }
 
-function renderAlbums() {
+function createAlbumItem(albums) {
+	for (let i = 0; i < albums.length; i++) {
+		const $albumItem = $(`<li data-id-number="${[i + 1]}">album ${albums[i].id}: ${albums[i].title}</li>`);
+		renderAlbums($albumItem);
+	}
+}
+
+function renderAlbums($albumItem) {
+	$albumList.append($albumItem);
+}
+
+function renderFirstAlbumPhotos() {
 	sendGetRequestAlbums()
-		.then((album) => {
-			for (let i = 0; i < album.length; i++) {
-				const albumItem = `<li data-id-number="${[i + 1]}">album ${album[i].id}: ${album[i].title}</li>`;
-				albumList.append(albumItem);
-			}
-		})
+		.then((response) => getAndRenderPhotos(response[0].id))
 }
 
-function getFirstAlbumId() {
-	return sendGetRequestAlbums()
-		.then((response) => renderPhotos(response[0].id))
-}
-
-function renderPhotos(albumId = getFirstAlbumId()) {
+function getAndRenderPhotos(albumId) {
 	fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${(albumId)}`)
 		.then((response) => response.json())
-		.then((photoList) => {
-			for (let i = 0; i < photoList.length; i++) {
-				const photo = `<li><img src="${photoList[i].url} alt="photo number ${photoList[i].id} from album ${albumId}"></li>`;
-				albumPhotos.append(photo);
-			}
-		})
+		.then((photoList) => createPhotoList(photoList))
+}
+
+function createPhotoList(photoList) {
+	for (let i = 0; i < photoList.length; i++) {
+		const $photo = $(`<li><img src="${photoList[i].url} alt="photo number ${photoList[i].id}"></li>`);
+		renderPhotos($photo);
+	}
+}
+
+function renderPhotos($photo) {
+	$albumPhotos.append($photo);
 }
 
 function renderAlbumPhotoEventListener() {
-	albumList.click((event) => {
+	$albumList.click((event) => {
 		const albumId = event.target.dataset.idNumber;
-		clearAlbum();
-		renderPhotos(albumId);
+		clearAlbumPhotos();
+		getAndRenderPhotos(albumId);
 	})
 }
 
-function clearAlbum() {
-	albumPhotos.empty();
+function clearAlbumPhotos() {
+	$albumPhotos.empty();
 }
